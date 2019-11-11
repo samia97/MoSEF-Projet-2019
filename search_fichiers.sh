4f95e4095a4d731aa3b8f9e7ee088c03d4e3880c
SamiaEzzabouri <samiaezzabouri97@gmail.com>
2019-11-11 13:35:24 +0100

    Ajout d'une requete supplementaire

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
  
-
+#créer une variable date sous le format jour/mois/année
 date=$(date +'%d %B %Y')
 
+#souhaiter la bienvenue à l'utilisateur qui utilise le script et lui afficher la date du jour
 echo 'Bienvenue' $USERNAME ',Nous sommes le ' $date
-echo "Quel répertoire vous interesse?"
 
-read directory 
+#demander à l'utilisateur de se mettre sur le répertoire qui l'intéresse
+
+echo "Quel répertoire vous interesse?"
+read directory
+
+
+#Retourner les fichiers selon le premier pattern defini par l'utilisateur
 
 ls $directory/$1
+
+#Afficher les fichiers selon le 2ème pattern, en fonction de notre résultat du pattern1 (en ignorant la casse)
+grep -il $2 $directory/$1
+
+
+ 
 
 
